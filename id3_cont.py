a333e0034130bff9e31da4109f65f53e60abbbf0
Gauthier Boeshertz <gauboes@ini.uzh.ch>
2020-05-24 15:56:29 +0200

    pioush

--- id3_cont.py
+++ id3_cont.py
@@ -28,24 +28,20 @@ class ID3_modif:
                     valeurs = set()
                     attributs[attribut] = valeurs
                 valeurs.add(float(valeur))
-        #attributs contient pour chaque attribut, toutes les valeurs qui apparaissent
         # Find the predominant class
         classes = set([row[0] for row in donnees])
-        # print(classes)
         predominant_class_counter = -1
         for c in classes:
-            # print([row[0] for row in donnees].count(c))
             if [row[0] for row in donnees].count(c) >= predominant_class_counter:
                 predominant_class_counter = [row[0] for row in donnees].count(c)
                 predominant_class = c
-        # print(predominant_class)
             
         arbre = self.construit_arbre_recur(donnees, attributs, predominant_class)
 
         return arbre
 
     def construit_arbre_recur(self, donnees, attributs, predominant_class):
-        """ Construit rédurcivement un arbre de décision à partir 
+        """ Construit récurcivement un arbre de décision à partir 
             des données d'apprentissage et d'un dictionnaire liant
             les attributs à la liste de leurs valeurs possibles.
             :param list donnees: les données d'apprentissage\
@@ -55,8 +51,6 @@ class ID3_modif:
             :return: une instance de NoeudDeDecision correspondant à la racine de\
             l'arbre de décision.
         """
-        #print('les attributs ici prennent ces valeurs : ', attributs)
-        #print('combien de données', len(donnees))
         def classe_unique(donnees):
             """ Vérifie que toutes les données appartiennent à la même classe. """
             
@@ -71,21 +65,16 @@ class ID3_modif:
         if donnees == []:
             return NoeudDeDecision(None, [str(predominant_class), dict()], str(predominant_class))
 
-        # Si toutes les données restantes font partie de la même classe,
-        # on peut retourner un noeud terminal.         
         elif classe_unique(donnees):
-            #print('on est au bout dune branche')
             return NoeudDeDecision(None, donnees, str(predominant_class))
             
         else:
-            #print('on va diviser avec : ', attribut, 'et comme valeur de partition : ', valeur_de_partition)
             min_attr, part_val = self.find_min_entr(donnees,attributs)
             
             new_attr_gauche = attributs.copy()
             new_attr_droite = attributs.copy()
 
             partitions = self.partitionne(donnees, min_attr, part_val)
-            #print('suite a ca, les partitions sont: ', partitions)
             enfants = {}
 
             newdroite = set()
@@ -95,22 +84,16 @@ class ID3_modif:
                         newdroite.add(x)
                     else:
                         newgauche.add(x)
-
-            minmaxlist= list(newdroite).append(list(newgauche))
+            
             new_attr_gauche[min_attr] =newgauche
             new_attr_droite[min_attr] = newdroite
-            #### pas mettre la valeur de partition
 
-            max_val = max(minmaxlist)
-            min_val = min(minmaxlist)
+            enfants["droite"] = self.construit_arbre_recur(partitions["droite"],new_attr_droite,predominant_class)
+            enfants["gauche"] = self.construit_arbre_recur(partitions["gauche"],new_attr_gauche,predominant_class)
 
-            enfants[tuple((part_val, max_val))] = self.construit_arbre_recur(partitions["droite"],new_attr_droite,predominant_class)
-            enfants[tuple((min_val, part_val))] = self.construit_arbre_recur(partitions["gauche"],new_attr_gauche,predominant_class)
+            return NoeudDeDecision(min_attr, donnees, str(predominant_class), enfants,part_val)
 
-
-            return NoeudDeDecision(min_attr, donnees, str(predominant_class), enfants)
-
-    def partitionne(self, donnees, attribut, valeur_de_partition):
+    def partitionne(self, donnees, attribut, val_part):
         """ Partitionne les données sur les valeurs a_j de l'attribut A.
             :param list donnees: les données à partitioner.
             :param attribut: l'attribut A de partitionnement.
@@ -124,7 +107,7 @@ class ID3_modif:
         
         for donnee in donnees:
             where =""
-            if donnee[1][attribut] < valeur_de_partition:
+            if donnee[1][attribut] < val_part:
                 where ="gauche"
             else :
                 where ="droite"
@@ -136,33 +119,8 @@ class ID3_modif:
 
         return partitions
 
-    def p_a_sup_val(self, donnees, attribut, valeur_de_partition):
-        """ p(a >= valeur_de_partition) - la probabilité que la valeur de l'attribut A soit >= a une valeur.
-            :param list donnees: les données d'apprentissage.
-            :param attribut: l'attribut A.
-            :param valeur_de_partition: la valeur de A seln laquelle on divise
-            :return: p(a >= valeur_de_partition)
-        """
-        # Nombre de données.
-        nombre_donnees = len(donnees)
-        
-        # Permet d'éviter les divisions par 0.
-        if nombre_donnees == 0:
-            return 0.0
-        
-        # Nombre d'occurrences de a_j >= valeur_de_partition parmi les données.
-        nombre_aj = 0
-        for donnee in donnees:
-            if float(donnee[1][attribut]) >= valeur_de_partition:
-                nombre_aj += 1
-
-        # p(a_j) = nombre d'occurrences de la valeur a_j parmi les données / 
-        #          nombre de données.
-        return nombre_aj / nombre_donnees
-
-    def p_ci_aj(self, donnees, attribut, valeur_de_partition, classe, sup_ou_egal = True):
+    def p_ci_aj(self, donnees, attribut, part_val, classe, where = "droite"):
         """ p(c_i|a_j) - la probabilité conditionnelle que la classe C soit c_i\
-            étant donné que l'attribut A >= ou < que valeur_de_partition.
             :param list donnees: les données d'apprentissage.
             :param attribut: l'attribut A.
             :param valeur: la valeur a_j de l'attribut A.
@@ -170,19 +128,23 @@ class ID3_modif:
             :return: p(c_i | a_j)
         """
         # Nombre d'occurrences de la valeur a_j parmi les données.
-        if sup_ou_egal:
-            donnees_aj = [donnee for donnee in donnees if float(donnee[1][attribut]) >= valeur_de_partition]
+        d_j = list()
+        if where =="droite":
+            for d in donnees:
+                if d[1][attribut]>= part_val:
+                    d_j.append(d)
         else:
-            donnees_aj = [donnee for donnee in donnees if float(donnee[1][attribut]) < valeur_de_partition]
-        nombre_aj = len(donnees_aj)
+            for d in donnees:
+                if d[1][attribut] < part_val:
+                    d_j.append(d)
+
+        nombre_aj = len(d_j)
         
         # Permet d'éviter les divisions par 0.
         if nombre_aj == 0:
             return 0
         
-        # Nombre d'occurrences de la classe c_i parmi les données pour lesquelles 
-        # A vaut >= ou < valeur_de_partition.
-        donnees_ci = [donnee for donnee in donnees_aj if donnee[0] == classe]
+        donnees_ci = [donnee for donnee in d_j if donnee[0] == classe]
         nombre_ci = len(donnees_ci)
 
         # p(c_i|a_j) = nombre d'occurrences de la classe c_i parmi les données 
@@ -190,21 +152,18 @@ class ID3_modif:
         #              nombre d'occurrences de la valeur a_j parmi les données.
         return nombre_ci / nombre_aj
 
-    def h_C_aj(self, donnees, attribut, valeur_de_partition, sup_ou_egal = True):
-        """ H(C|a >) - l'entropie de la classe parmi les données pour lesquelles\
-            l'attribut A vaut a_j.
+    def h_C_aj(self, donnees, attribut, valeur_de_partition, where = "droite"):
+        """ l'entropie de la classe parmi les données pour lesquelles\
             :param list donnees: les données d'apprentissage.
             :param attribut: l'attribut A.
             :param valeur_de_partition: la valeur de partition pour l'attribut A.
-            :param sup_ou_egal: dit si on cherche H(C|a>=valeur_de_partition) ou H(C|a<valeur_de_partition)
-            :return: H(C|a_j)
         """
         # Les classes attestées dans les exemples.
         classes = list(set([donnee[0] for donnee in donnees]))
         
         # Calcule p(c_i|a_j) pour chaque classe c_i.
 
-        p_ci_ajs = [self.p_ci_aj(donnees, attribut, valeur_de_partition, classe, sup_ou_egal)
+        p_ci_ajs = [self.p_ci_aj(donnees, attribut, valeur_de_partition, classe, where)
                     for classe in classes]
 
         # Si p vaut 0 -> plog(p) vaut 0.
@@ -212,7 +171,7 @@ class ID3_modif:
                     for p_ci_aj in p_ci_ajs 
                     if p_ci_aj != 0])
 
-    def h_C_A(self, donnees, attribut, valeur_de_partition):
+    def h_C_A(self, donnees, attribut, part_val):
         """ H(C|A) - l'entropie de la classe après avoir choisi de partitionner\
             les données suivant les valeurs de l'attribut A.
             
@@ -221,22 +180,25 @@ class ID3_modif:
             :param list valeur_de_partition: la valeur de l'attribut A selon laquelle on divise.
             :return: H(C|A)
         """
-        # calcule p(a >= valeur_de_partition) et p(a < valeur_de_partition). ou a est la valeur de l'attribut A
-        p_a_sup = self.p_a_sup_val(donnees, attribut,valeur_de_partition)
-        p_a_inf = 1-p_a_sup
-        p_as = [p_a_sup, p_a_inf]
+        
+        
+        prob_less = 0
+        
+        for donnee in donnees:
+            if donnee[1][attribut] < part_val:
+                prob_less += 1/len(donnees)
 
-        # Calcule H(C|a>=valeur_de_partition) et H(C|a<valeur_de_partition)
-        h_C_a_sup = self.h_C_aj(donnees, attribut, valeur_de_partition, sup_ou_egal=True)
-        h_C_a_inf = self.h_C_aj(donnees, attribut, valeur_de_partition, sup_ou_egal=False)
-        h_c_as = [h_C_a_sup, h_C_a_inf]
+        p = [prob_less, 1-prob_less]
 
-        return sum([p_a * h_c_a for p_a, h_c_a in zip(p_as, h_c_as)])
+        h_more = self.h_C_aj(donnees, attribut, part_val)
+        h_less = self.h_C_aj(donnees, attribut, part_val, where="gauche")
+        h = [ h_less,h_more]
+
+        return sum([p_a * h_a for p_a, h_a in zip(p, h)])
 
 
    
     def find_min_entr(self,donnees,attributs):
-                    # Sélectionne l'attribut qui réduit au maximum l'entropie.
             
         all_entropies = list()
         min_entr = 1e6
@@ -250,8 +212,6 @@ class ID3_modif:
                     min_entr=entr
                     min_attr =a
                     min_val = v
-                    
-                    
 
         return min_attr,min_val
 
